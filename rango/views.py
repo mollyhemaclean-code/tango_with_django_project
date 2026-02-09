5494c9bb65ecb1d66d814dbfbb08ad165da2884b
Molly Maclean <3022224M@student.gla.ac.uk>
2026-02-09 23:59:07 +0000

    chapter 4 exercises commit

--- rango/views.py
+++ rango/views.py
@@ -7,7 +7,5 @@ def index(request):
     
 
 def about(request):
-    return HttpResponse(
-        "Rango says here is the about page."
-        "<a href='/rango/'>Index</a>"
-    )
+    context_dict = {'boldmessage': 'This tutorial has been put together by Molly Maclean.'}
+    return render(request, 'rango/about.html', context=context_dict)
