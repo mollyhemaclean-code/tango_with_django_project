aa93864e06e68a1a0d0bcabe6d72191409f5169f
Molly Maclean <3022224M@student.gla.ac.uk>
2026-02-13 01:02:47 +0000

    chapter 9 exercises commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -102,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -116,6 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+LOGIN_URL = 'rango:login'
 
 TIME_ZONE = 'GMT'
 
